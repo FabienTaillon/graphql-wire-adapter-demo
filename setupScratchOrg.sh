f093247258ca423670dc47797fee3d9c3f23f60b
Fabien Taillon <ftaillon@gmail.com>
2023-10-18 14:46:15 +0200

    Moved to sf project deploy start

--- setupScratchOrg.sh
+++ setupScratchOrg.sh
@@ -6,7 +6,7 @@ PROJECT_NAME=${PWD##*/}
 SCRATCH_ALIAS="${SCRATCH_ALIAS:-$PROJECT_NAME-`git branch | grep \* | cut -d ' ' -f2`}" 
 sf org create scratch --definition-file config/project-scratch-def.json --alias $SCRATCH_ALIAS --set-default --wait 10
 echo '##### PUSHING METADATA #####'
-sf force source push --target-org $SCRATCH_ALIAS
+sf project deploy start --source-dir force-app --target-org $SCRATCH_ALIAS
 echo '##### ASSIGNING PERMISSIONS #####'
 sfdx force user permset assign --perm-set-name DreaminEvent --target-org $SCRATCH_ALIAS
 echo '##### IMPORTING DUMMY DATA #####'
