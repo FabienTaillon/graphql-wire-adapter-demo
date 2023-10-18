a0a686d06d60236c065a2f1b7387c8ea2f510cac
Fabien Taillon <ftaillon@gmail.com>
2023-10-18 14:37:11 +0200

    Moved to sf v2

--- setupScratchOrg.sh
+++ setupScratchOrg.sh
@@ -4,16 +4,16 @@ echo '##### CREATING SCRATCH ORG #####'
 SCRATCH_ALIAS=$1
 PROJECT_NAME=${PWD##*/}
 SCRATCH_ALIAS="${SCRATCH_ALIAS:-$PROJECT_NAME-`git branch | grep \* | cut -d ' ' -f2`}" 
-sfdx org create scratch --definition-file config/project-scratch-def.json --alias $SCRATCH_ALIAS --set-default --wait 10
+sf org create scratch --definition-file config/project-scratch-def.json --alias $SCRATCH_ALIAS --set-default --wait 10
 echo '##### PUSHING METADATA #####'
-sfdx force:source:push --target-org $SCRATCH_ALIAS
+sf force source push --target-org $SCRATCH_ALIAS
 echo '##### ASSIGNING PERMISSIONS #####'
-sfdx force:user:permset:assign --perm-set-name DreaminEvent --target-org $SCRATCH_ALIAS
+sfdx force user permset assign --perm-set-name DreaminEvent --target-org $SCRATCH_ALIAS
 echo '##### IMPORTING DUMMY DATA #####'
-sfdx texei:data:import --inputdir ./data --targetusername $SCRATCH_ALIAS
+sf texei data import --inputdir ./data --target-org $SCRATCH_ALIAS
 echo '##### CLEANING STANDARD LAYOUTS #####'
-sfdx texei:source:layouts:cleanorg --targetusername $SCRATCH_ALIAS
+sf texei source layouts cleanorg --target-org $SCRATCH_ALIAS
 echo '##### ENABLE LWC DEBUG MODE #####'
-sf texei debug lwc enable
+sf texei debug lwc enable --target-org $SCRATCH_ALIAS
 echo '##### OPENING SCRATCH ORG #####'
-sfdx org:open --path lightning/n/Dreamin_Event_Explorer --target-org $SCRATCH_ALIAS
+sf org open --path lightning/n/Dreamin_Event_Explorer --target-org $SCRATCH_ALIAS
